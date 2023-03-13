d0beed0181f471bdb51638c774492106680012b5
jban28 <jd.bannister28@gmail.com>
2023-03-13 12:24:54 +0000

    Initialise repo

---
+++ src/components/navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-sm">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.html" style="color:black">MATT PAGETT</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+      data-bs-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav w-100 justify-content-between">
+          <li class="nav-item hide-sm">
+            <a class="nav-link active" style="padding-left: 0px;" href="index.html">MATT PAGETT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="bodies.html">Bodies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="flowers.html">Flowers</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="books.html">Books</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="padding-right: 0px" href="about.html">About/Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+  @import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css';
+</style>
