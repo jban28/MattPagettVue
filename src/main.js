988be41cab98aba4567fbaf90110841968a0a447
jban28 <jd.bannister28@gmail.com>
2023-10-25 10:26:53 +0100

    google analytics tag added

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import "bootstrap";
 import { createApp } from 'vue'
 import App from './App.vue'
 import routerPromise from './router/index.js'
+import VueGtag from "vue-gtag";
 import './assets/main.css'
 
 routerPromise.then(router => {
   createApp(App)
   .use(router)
+  .use(VueGtag, {
+    config: { id: "G-BM3HHQKEE2" }
+  }, router)
   .mount('#app')
 })
 
