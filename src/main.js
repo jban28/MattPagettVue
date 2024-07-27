f495a29f156d4306759710a6333a95fa647d0571
jban28 <91541400+jban28@users.noreply.github.com>
2024-07-27 07:44:58 +0100

    amed createApp to remove router promise

--- src/main.js
+++ src/main.js
@@ -2,17 +2,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import { createApp } from 'vue'
 import App from './App.vue'
-import routerPromise from './router/index.js'
+//import routerPromise from './router/index.js'
+import router from './router/index.js';
 import VueGtag from "vue-gtag";
 import './assets/main.css'
 
-routerPromise.then(router => {
-  createApp(App)
-  .use(router)
-  .use(VueGtag, {
-    config: { id: "G-BM3HHQKEE2" }
-  }, router)
-  .mount('#app')
-})
+// routerPromise.then(router => {
+//   createApp(App)
+//   .use(router)
+//   .use(VueGtag, {
+//     config: { id: "G-BM3HHQKEE2" }
+//   }, router)
+//   .mount('#app')
+// })
 
 
+createApp(App)
+.use(router)
+.use(VueGtag, {
+  config: { id: "G-BM3HHQKEE2" }
+}, router)
+.mount('#app')
