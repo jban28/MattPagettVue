694c71808a8f9145742fe8f6cad4de44c4715307
jban28 <jd.bannister28@gmail.com>
2024-09-26 11:41:02 +0100

    restructure new-image view with css grid

--- src/views/new-image.vue
+++ src/views/new-image.vue
@@ -1,91 +1,123 @@
 <script setup>
   import { auth } from '../scripts/token';
   import { useRouter } from 'vue-router';
-  import {ref} from 'vue';
+  import { ref } from 'vue';
 
   const props = defineProps({
     allImages: Object
   })
 
-  let router = useRouter();
-  let autoThumb = ref(true);
+  const name = ref(null);
+  const caption = ref(null);
+  const series = ref(null);
 
-  let createImage = function () {
-    let postData = new FormData();
+  const router = useRouter();
 
-    postData.append("file", document.getElementById("select-image").files[0]);
-    postData.append("name", document.getElementById("img-name").value)
-    postData.append("caption", document.getElementById("caption").value)
-    postData.append("series", document.getElementById("series").value)
+  const handleSumbit = () => {
+    console.log('submit')
+  }
+  // let createImage = function () {
+  //   let postData = new FormData();
 
-    if (!document.getElementById("select-thumb").disabled){
-      postData.append("thumb", document.getElementById("select-thumb").files[0]);
-    }
+  //   postData.append("file", document.getElementById("select-image").files[0]);
+  //   postData.append("name", document.getElementById("img-name").value)
+  //   postData.append("caption", document.getElementById("caption").value)
+  //   postData.append("series", document.getElementById("series").value)
+
+  //   if (!document.getElementById("select-thumb").disabled){
+  //     postData.append("thumb", document.getElementById("select-thumb").files[0]);
+  //   }
     
-    fetch('https://artistapi.bannisterwebservices.co.uk/image',
-      {
-        method: 'POST',
-        headers: {
-          'Authorization': auth.token
-        },
-        body: postData
-      })
-      .then(async http => {
-        let content = http.json();
-        if (http.ok) {
-          return content;
-        }
-        else {
-          return content.then(content => {throw new Error(content);})
-        }
-      })
-      .then(newImage =>{
-        console.log(newImage);
-        props.allImages[newImage.series].unshift(newImage);
-        router.push({path: "/admin/edit"})
-      })
-      .catch(error => {
-        console.log(error);
-      })
-    }
+  //   fetch('https://artistapi.bannisterwebservices.co.uk/image',
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         'Authorization': auth.token
+  //       },
+  //       body: postData
+  //     })
+  //     .then(async http => {
+  //       let content = http.json();
+  //       if (http.ok) {
+  //         return content;
+  //       }
+  //       else {
+  //         return content.then(content => {throw new Error(content);})
+  //       }
+  //     })
+  //     .then(newImage =>{
+  //       console.log(newImage);
+  //       props.allImages[newImage.series].unshift(newImage);
+  //       router.push({path: "/admin/edit"})
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     })
+  //   }
 </script>
 
 <template>
-  <div class="new-image">
-    <label class="new-image" for="select-image">Select Image:</label>
-    <input id="select-image" name="image" type="file"/><br/>
+  <div class="new-image-form">
+    <label for="image">Select Image:</label>
+    <input id="select-image" name="image" type="file"/>
 
-    <label class="new-image" for="auto-thumb">Autogenerate thumbnail?</label>
-    <input id="auto-thumb" type="checkbox" :checked=autoThumb @change="autoThumb=!autoThumb"/><br/>
+    <label for="name">Name:</label>
+    <input v-model="name" name="name" type="text"/>
 
-    <label class="new-image" for="select-thumb">Select Thumbnail:</label>
-    <input id="select-thumb" name="thumb" type="file" :disabled="autoThumb"/><br/>
+    <label for="caption">Caption:</label>
+    <textarea v-model="caption" name="caption" rows="5"></textarea>
 
-    <label class="new-image" for="img-name">Name:</label>
-    <input id="img-name" type="text"/><br/>
-
-    <label class="new-image" for="caption">Caption:</label>
-    <textarea id="caption" rows="5"></textarea><br/>
-
-    <label class="new-image" for="series">Series:</label>
-    <select id="series">
+    <label for="series">Series:</label>
+    <select v-model="series" name="series">
       <option value="bodies">Bodies</option>
       <option value="flowers">Flowers</option>
       <option value="designs">Designs</option>
       <option value="plates">Plates</option>
-    </select><br/>
-    
-    <button style="margin-left: 112px" @click="createImage">Submit</button>
-    <button @click="router.push({path: '/admin/edit'})">Cancel</button>
+    </select>
+
+    <span class="btn-cell" >
+      <button  @click="handleSumbit">Submit</button>
+      <button @click="router.push({path: '/admin/edit'})">Cancel</button>
+    </span>
   </div>
 </template>
 
 <style scoped>
-  .new-image label {
-    width: 124px;
+  .new-image-form {
+    display: grid;
+    grid-template-columns: max-content 1fr;
+    gap: 12px;
+    width: 100%;
+    max-width: 812px;
+    margin: auto;
   }
 
-  .new-image textarea {
-    width: calc(100% - 136px) 
+  .btn-cell {
+    grid-column: 2 / 2;
+  }
+
+  @media screen and (max-width: 30rem) {
+    .new-image-form {
+      display: flow;
+    }
+
+    .new-image-form > * {
+      display: block;
+      width: 100%;
+    }
+
+    .new-image-form > label {
+      margin-top: 12px;
+    }
+
+    .new-image-form > label:first-of-type {
+      margin-top: 0;
+    }
+
+    .btn-cell {
+      margin-top: 24px;
+      display: flex;
+      justify-content: space-evenly;
+    }
   }
 </style>
